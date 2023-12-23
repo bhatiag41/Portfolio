738f916f1e969c50da0729e62db97acef688d01d
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2023-12-23 21:11:20 +0530

    pta ni

--- src/Components/Hero2.js
+++ src/Components/Hero2.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Hero2 = (props) => {
   return (
-    <div className="hero">
-        <div className="heading">
+    <div className="hero2">
+        <div className="heading2">
             <h1>{props.heading}</h1>
             <p>{props.text}</p>
         </div>
--- src/Components/workdata.js
+++ src/Components/workdata.js
@@ -3,24 +3,33 @@
 export default[
     {
         ProjectTitle:"Inventory Management System",
-        ProjectText:"",
+        ProjectText:"An Inventory Management System using ReactJS and Firebase to help our college keep track of the inventory",
         ViewURL:"",
+        SourceURL:"",
+        img:"../../Assets/ecosavvy.png",
+    
+    },
+    {
+        ProjectTitle:"EcoSavvy",
+        ProjectText:"A project based on sustainable development that offers a chatBOT and evnviornment related news articles",
+        ViewURL:"",
+        SourceURL:"",
+        img:'../../Assets/ecosavvy.png',
+    
+    },
+    {
+        ProjectTitle:"Drum Kit",
+        ProjectText:"",
+        ViewURL:"https://tranquil-semolina-ceda71.netlify.app/",
         SourceURL:"",
         img:'',
     
     },
     {
-        ProjectTitle:"EcoSavvy",
+        ProjectTitle:"Maze Game",
         ProjectText:"",
-        ViewURL:"",
+        ViewURL:"https://glittering-mousse-ffd07e.netlify.app/",
         SourceURL:"",
-    
-    },
-    {
-        ProjectTitle:"",
-        ProjectText:"",
-        ViewURL:"",
-        SourceURL:"",
-    
+        img:'',
     },
 ]
